5725e08160b05b9970fa41796bd42ab804acaad6
Jesse <jesse@archangel.wtf>
2023-02-08 13:46:55 +0100

    Initial Commit

---
+++ ToolbarItemSample/AppShell.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Input;
+
+namespace ToolbarItemSample;
+
+public partial class AppShell : Shell
+{
+    public ICommand FirstCommand { get; set; }
+    public ICommand SecondCommand { get; set; }
+
+    public AppShell()
+	{
+        FirstCommand = new Command((parameter) =>
+        {
+            Console.WriteLine("First!");
+        });
+
+        SecondCommand = new Command((parameter) =>
+        {
+            Console.WriteLine("Second!");
+        });
+
+
+        InitializeComponent();
+	}
+}
